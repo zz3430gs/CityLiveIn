1111a27873f23cc9fbf16f0d3b4ba513f49de231
Joe Lee <zz3430gs@t3040-07.campus.minneapolis.edu>
2016-08-30 16:33:16 -0500

    Initial commit

---
+++ app/src/main/java/com/joe/citylivein/MainActivity.java
@@ -0,0 +1,30 @@
+package com.joe.citylivein;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText nameTextBox = (EditText) findViewById(R.id.city_text_box);
+        final EditText cityTextBox = (EditText) findViewById(R.id.city_search_box);
+        Button cityTextButton = (Button) findViewById(R.id.city_search_button);
+        cityTextButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                String nameSearchString = nameTextBox.getText().toString();
+                String citySearchString = cityTextBox.getText().toString();
+
+            }
+        });
+    }
+}
